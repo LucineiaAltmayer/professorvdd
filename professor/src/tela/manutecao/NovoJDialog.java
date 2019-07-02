051a7be3ba0487752f9362c5b173877661779a3e
Administrador <Administrador@10.10.4.3>
2019-07-02 16:46:30 -0300

    mdsssssssss

--- professor/src/tela/manutecao/NovoJDialog.java
+++ professor/src/tela/manutecao/NovoJDialog.java
@@ -146,7 +146,7 @@ public class NovoJDialog extends javax.swing.JDialog {
     }//GEN-LAST:event_jtfNomeActionPerformed
 
     private void btnAdicionarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAdicionarActionPerformed
-     controlador.Controladorprofessor.inserir(this);   // TODO add your handling code here:
+     controlador.ControladorProfessor.inserir(this);   // TODO add your handling code here:
     }//GEN-LAST:event_btnAdicionarActionPerformed
 
     /**
